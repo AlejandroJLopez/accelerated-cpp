9b67d9472d5ceacfb546458f1a897b99c7067a96
alejandro <alejandro@alejandro.casa>
2018-05-08 21:08:49 +0200

    test

---
+++ 2/2-0.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char const *argv[])
+{
+	std::cout << "Please enter your first name: ";
+
+	std::string name;
+	std::cin >> name;
+
+	cont std::string greeting = "Hello, " + name + "!";
+
+	
+
+	return 0;
+}
